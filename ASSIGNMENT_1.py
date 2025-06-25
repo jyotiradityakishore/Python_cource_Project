56abe120d0c06ebb61b15e29fc96f301fcdfa53e
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-06-26 00:12:08 +0530

    Add files via upload
    This progrm takes value from user and porforms basic arithmetic calculation at ones and displayes to user

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,16 @@
+
+
+print("Enter your Numbe :")
+a = int(input())
+
+print("Enter your Second Numbe :")
+b = int(input())
+
+c = a + b
+d = a - b
+e = a * b
+f = a/ b
+print("Addition",c )
+print("Subtraction",d )
+print("Multiplication",e)
+print("Division",f)

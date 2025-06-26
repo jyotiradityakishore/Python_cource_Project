f80a855d9d9b5a13e890d10463fbb2cfebfe634a
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-06-26 16:35:46 +0530

    Add files via upload

---
+++ ASSIGNMENT_1_Q2.py
@@ -0,0 +1,9 @@
+
+
+print("Enter Your First Name")
+First_Name = input()
+print("Enter Your Last Name")
+Last_Name = input()
+
+Full_Name = First_Name + " " + Last_Name
+print("Hello",Full_Name,"! Welcome To Python Program")

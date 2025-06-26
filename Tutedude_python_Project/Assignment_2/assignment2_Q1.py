e753f0a52631de070b7c8a9859e401e2ec509aec
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-06-27 03:08:32 +0530

    Add files via upload

---
+++ Tutedude_python_Project/Assignment_2/assignment2_Q1.py
@@ -0,0 +1,8 @@
+
+print("Enter Your Number:")
+User_number = int(input())
+
+if User_number%2 == 0:
+    print(User_number,"Is an even number")
+else:
+    print(User_number," Is an odd number ")
---
+++ Tutedude_python_Project/Assignment_2/assignment2_Q2.py
@@ -0,0 +1,10 @@
+
+
+Total = 0
+
+for i in range(1,51,1):
+  Total += i
+
+print("The sum of the Number from 0 to 50 is:",Total)
+
+

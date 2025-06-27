4b1bdf58867dd789cc9e81d73bb374e8596005c7
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-06-27 23:04:03 +0530

    Update assigment4_Q1.py

--- Tutedude_python_Project/assignment_4/Q1/assigment4_Q1.py
+++ Tutedude_python_Project/assignment_4/Q1/assigment4_Q1.py
@@ -1,21 +1,21 @@
 
-def Read():
-
+def Read():                       
       Sample_File = open("Sample.txt","r")
-      for line in Sample_File:
+      for line in Sample_File:                                                # reading content of file
         print(line)
       Sample_File.close()
       return line
 
-def CountLines():
+def CountLines():                   
    try:
     Sample_File = open("Sample.txt","r")
     Line_Count = 0
-    for line in Sample_File:
+    for line in Sample_File:                                                #counting lines of file and printing output & checking for file
       Line_Count += 1
       print("line",Line_Count,":", line)
     Sample_File.close()
    except FileNotFoundError:
      print("\nthe File Sample.txt was not found")
 
-CountLines()
+
+CountLines()                                                                # initializing output through function

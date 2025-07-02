4482214fb7aeafbec9a55a51038326058f1d0fae
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-07-02 17:01:55 +0530

    Add files via upload

---
+++ Tutedude_python_Project/Assigment_5/assigment5_Q2.py
@@ -0,0 +1,13 @@
+
+
+Full_List  = [i for i in range(1,11)]
+print("Original List :",Full_List)
+
+List_Slice = Full_List[0:5]
+print("Extrated first five element:",List_Slice)
+
+Reversed_List = Full_List[0:5][::-1]
+print("Reversed extrated element :",Reversed_List)
+
+
+

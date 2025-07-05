4ea2b9e44d6a98e3a9b3823f7965cf9d2d398e8f
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-07-05 23:55:07 +0530

    Update assigment5_Q2.py

--- Tutedude_python_Project/Assigment_5/assigment5_Q2.py
+++ Tutedude_python_Project/Assigment_5/assigment5_Q2.py
@@ -1,14 +1,14 @@
 
 
-Full_List  = [i for i in range(1,11)]
+Full_List  = [i for i in range(1,11)]              #creating list from 1 to 10
 print("Original List :",Full_List)
 
 
-List_Slice = Full_List[0:5]
+List_Slice = Full_List[0:5]                         #sliceing list as per our need
 print("Extrated first five element:",List_Slice)
 
 
-Reversed_List = Full_List[0:5][::-1]
+Reversed_List = Full_List[0:5][::-1]                  # reverceing the list 
 print("Reversed extrated element :",Reversed_List)
 
 

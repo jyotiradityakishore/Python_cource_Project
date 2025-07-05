2e47ff3f544c7b002d8885df2938aa6352ac8dbd
jyotiradityakishore <87024800+jyotiradityakishore@users.noreply.github.com>
2025-07-05 23:52:20 +0530

    Update assigment5_Q1.py

--- Tutedude_python_Project/Assigment_5/assigment5_Q1.py
+++ Tutedude_python_Project/Assigment_5/assigment5_Q1.py
@@ -1,11 +1,11 @@
 
 
-Student_detal = {"jon":34,"akash":64, "sita":73, "mohan":94}      #
-User_Input = input("Enter the student's name: ")                  #
+Student_detal = {"jon":34,"akash":64, "sita":73, "mohan":94}      #  student detal 
+User_Input = input("Enter the student's name: ")                  #  taking input from  user
 
 
 if User_Input in Student_detal:
-    print(User_Input,"Marks:",Student_detal[User_Input])          #
+    print(User_Input,"Marks:",Student_detal[User_Input])          # compairing user input to student_detal
 else:
     print(User_Input,"Student Not found")
 
